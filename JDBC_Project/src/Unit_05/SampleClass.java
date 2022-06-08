fc30c6ae139926e8c2e5c5cd50d2cbd5fe02e45e
Pratham Kashyap <Pratham Kashyap@LAPTOP-SM8BOTMO>
2022-06-09 00:20:05 +0530

    JDBC Connection with MySQL Server

--- JDBC_Project/src/Unit_05/SampleClass.java
+++ JDBC_Project/src/Unit_05/SampleClass.java
@@ -7,18 +7,24 @@ public class SampleClass {
 		Connection myConn = null;
 		Statement myStmt = null;
 		ResultSet myRs = null;
-		try {
+		try 
+		{
 			myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbcDatabase", "root", "Pratham2002@SQL");
 			myStmt = myConn.createStatement();
 			int a = myStmt.executeUpdate("INSERT INTO jdbc_tbl VALUES (5, 'Mysql','Akbar', '2021-02-18')");
 			myRs = myStmt.executeQuery("select * from jdbc_tbl");
-			while (myRs.next()) {
+			while (myRs.next())
+			{
 				System.out.println(myRs.getString("jdbc_id") + ", " + myRs.getString("title") + ", "
 						+ myRs.getString("author") + ", " + myRs.getString("submission_date"));
 			}
-		} catch (Exception exc) {
+		} 
+		catch (Exception exc) 
+		{
 			exc.printStackTrace();
-		} finally {
+		} 
+		finally 
+		{
 			myRs.close();
 			myStmt.close();
 			myConn.close();
